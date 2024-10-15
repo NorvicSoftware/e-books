58605c968c6996f3a5e4ab8617dd2449bffcffba
Victor <victor@Victors-MacBook-Pro.local>
2024-10-14 21:29:46 -0400

    Correcion de las migraciones

--- database/migrations/2024_10_14_034421_create_scors_table.php
+++ database/migrations/2024_10_14_034421_create_scors_table.php
@@ -15,12 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('score', 15);
             $table->date('score_date');
-            $table->unsignedBigInteger('book_id');
-            $table->unsignedBigInteger('author_id');
+            // $table->unsignedBigInteger('book_id');
+            // $table->unsignedBigInteger('author_id');
+            $table->morphs('scoreable');
             $table->timestamps();
 
-            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
-            $table->foreign('score_date')->references('id')->on('authors')->onDelete('cascade');
+            // $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
+            // $table->foreign('score_date')->references('id')->on('authors')->onDelete('cascade');
         });
     }
 
--- database/migrations/2024_10_14_202118_create_sales_table.php
+++ database/migrations/2024_10_14_202118_create_sales_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->date('sale_date');
             $table->timestamps();
 
-            $table->foreign('customers_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->foreign('promotion_id')->references('id')->on('promotions')->onDelete('cascade');
         });
     }
